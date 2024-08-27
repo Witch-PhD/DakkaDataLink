d882fbe7c14a19e809cb1e2080805326887e69d0
Witch-PhD <fifteenthprime@gmail.com>
2024-08-27 18:05:50 -0500

    Adds MLT artillery profile

--- PitBoss/ArtilleryProfiles.cs
+++ PitBoss/ArtilleryProfiles.cs
@@ -31,6 +31,7 @@ namespace PitBoss
                 { Exalt150mm.ProfileName, Exalt150mm },
                 { Skycaller.ProfileName, Skycaller },
                 { Cremari_Mortar.ProfileName, Cremari_Mortar },
+                { Mortar_Light_Tank.ProfileName, Mortar_Light_Tank },
             };
             CurrentProfile = ArtyProfilesDict["Lariat 120mm"];
         }
@@ -65,11 +66,13 @@ namespace PitBoss
         public readonly ArtilleryProfile Exalt150mm = new ArtilleryProfile(EXALT_150_NAME, 8.0, 100.0, 300.0);
         public readonly ArtilleryProfile Skycaller = new ArtilleryProfile(SKYCALLER_NAME, 15.0, 200.0, 275.0);
         public readonly ArtilleryProfile Cremari_Mortar = new ArtilleryProfile(CREMARI_MORTAR_NAME, 0.5, 45.0, 80.0);
+        public readonly ArtilleryProfile Mortar_Light_Tank = new ArtilleryProfile(MORTAR_LIGHT_TANK_NAME, 2.3, 45.0, 80.0);
 
         public const string LARIAT_120_NAME = "Lariat 120mm";
         public const string EXALT_150_NAME = "Exalt 150mm";
         public const string SKYCALLER_NAME = "Skycaller";
         public const string CREMARI_MORTAR_NAME = "Cremari Mortar";
+        public const string MORTAR_LIGHT_TANK_NAME = "Devitt Mortar Tank (MLT)";
 
         public class ArtilleryProfile
         {
