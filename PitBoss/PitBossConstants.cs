29c14f3eb4d014305d90abab30edab0f2e8a52d4
TheWitchDoctor <82695226+Witch-PhD@users.noreply.github.com>
2024-09-11 20:38:15 -0500

    Increments version to 0.3.4

--- PitBoss/PitBossConstants.cs
+++ PitBoss/PitBossConstants.cs
@@ -2,7 +2,7 @@
 {
     public static class PitBossConstants
     {
-        public static readonly string VERSION_STRING = "0.3.2";
+        public static readonly string VERSION_STRING = "0.3.4";
         public static int SERVER_PORT = 50082;
         //public enum ConnectionStatus
         //{
