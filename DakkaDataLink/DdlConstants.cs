90125cbc718b566d8564569c73b7dbde4f766047
TheWitchDoctor <82695226+Witch-PhD@users.noreply.github.com>
2024-09-25 06:25:11 -0500

    Increments version to 0.4.1

--- DakkaDataLink/DdlConstants.cs
+++ DakkaDataLink/DdlConstants.cs
@@ -2,7 +2,7 @@
 {
     public static class DdlConstants
     {
-        public static readonly string VERSION_STRING = "0.4.0";
+        public static readonly string VERSION_STRING = "0.4.1";
         public static int SERVER_PORT = 50082;
         public static int REMOTE_USER_TIMEOUT_MILLISECONDS = 5000;
         //public enum ConnectionStatus
