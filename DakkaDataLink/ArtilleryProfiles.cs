d0366e9a4fa9222695f93466be8e56a69f33fb69
TheWitchDoctor <82695226+Witch-PhD@users.noreply.github.com>
2024-11-04 09:44:33 -0600

    Adds Thunderbolt 150mm artillery profile.

--- DakkaDataLink/ArtilleryProfiles.cs
+++ DakkaDataLink/ArtilleryProfiles.cs
@@ -24,6 +24,7 @@
                 { Lariat120mm.ProfileName, Lariat120mm },
                 { Exalt150mm.ProfileName, Exalt150mm },
                 { Flood150mmSpg.ProfileName, Flood150mmSpg },
+                { Thunderbolt150mm.ProfileName, Thunderbolt150mm },
                 { Skycaller.ProfileName, Skycaller },
                 { WaspNest.ProfileName, WaspNest },
                 { Cremari_Mortar.ProfileName, Cremari_Mortar },
@@ -62,6 +63,7 @@
         public readonly ArtilleryProfile Lariat120mm = new ArtilleryProfile(LARIAT_120_NAME, 8.0, 100.0, 300.0);
         public readonly ArtilleryProfile Exalt150mm = new ArtilleryProfile(EXALT_150_NAME, 8.0, 100.0, 300.0);
         public readonly ArtilleryProfile Flood150mmSpg = new ArtilleryProfile(FLOOD_SPG_150_NAME, 7.6, 120.0, 250.0);
+        public readonly ArtilleryProfile Thunderbolt150mm = new ArtilleryProfile(THUNDERBOLT_150_NAME, 6.0, 200.0, 350.0);
         public readonly ArtilleryProfile Skycaller = new ArtilleryProfile(SKYCALLER_NAME, 15.0, 200.0, 275.0);
         public readonly ArtilleryProfile WaspNest = new ArtilleryProfile(WASP_NEST_NAME, 10.0, 225.0, 325.0);
         public readonly ArtilleryProfile Cremari_Mortar = new ArtilleryProfile(CREMARI_MORTAR_NAME, 0.5, 45.0, 80.0);
@@ -69,8 +71,9 @@
         public readonly ArtilleryProfile Blacksteele_Frigate = new ArtilleryProfile(BLACKSTEELE_FRIGATE_NAME, 4.5, 100.0, 200.0);
 
         public const string LARIAT_120_NAME = "Lariat 120mm Gun";
-        public const string EXALT_150_NAME = "Exalt 150mm Gun";
+        public const string EXALT_150_NAME = "Exalt 150mm Gun (Warden)";
         public const string FLOOD_SPG_150_NAME = "Flood 150mm SPG";
+        public const string THUNDERBOLT_150_NAME = "Thunderbolt 150mm Gun (Colonial)";
         public const string SKYCALLER_NAME = "Skycaller (Rocket Halftrack)";
         public const string WASP_NEST_NAME = "Wasp Nest (Towed Rocket)";
         public const string CREMARI_MORTAR_NAME = "Cremari Mortar";
